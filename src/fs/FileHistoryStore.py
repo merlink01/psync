f8bcd9e3320295ad16125248a52b3699ea389a2a
Peter Thatcher <pthatcher@gmail.com>
2011-02-18 00:20:58 -0700

    Add fs/FileHistoryStore.py

---
+++ src/fs/FileHistoryStore.py
@@ -0,0 +1,53 @@
+# Copyright 2006 Uberan - All Rights Reserved
+
+import sqlite3
+
+from util import Record, sql, into
+
+TABLE_NAME = "files"
+TABLE_FIELDS = ["path", "utime", "size", "mtime", "hash"]
+
+class FileHistoryEntry(Record("path", "utime", "size", "mtime", "hash")):
+    pass
+
+class FileHistoryStore(Record("db_conn")):
+    def __new__(cls, db_conn):
+        create_table(db_conn)
+        return cls.new(db_conn)
+
+    #*** user peerid
+    def read_entries(self):
+        return select_entries(self.db_conn)
+
+    #*** user peerid
+    def add_entries(self, entries):
+        return insert_entries(self.db_conn, entries)
+
+def create_table(db_conn):
+    db_cursor = db_conn.cursor()
+    try:
+        db_cursor.execute(sql.create_table(
+                "files", ["path varchar",
+                          "utime integer",
+                          "size integer",
+                          "mtime integer",
+                          "hash varchar"]))
+        db_conn.commit()
+    except sqlite3.OperationalError:
+        pass  # Already exists?
+
+# Reads 100,000/sec on my 2008 Macbook.  If you sort by utime, it goes
+# down to 40,000/sec, so that doesn't seem like a good idea.
+@into(list)
+def select_entries(db_conn):
+    db_cursor = db_conn.cursor()
+    for (path, utime, size, mtime, hash) in \
+            db_cursor.execute(sql.select(TABLE_NAME, TABLE_FIELDS)):
+        yield FileHistoryEntry(path, utime, size, mtime, hash)
+
+def insert_entries(db_conn, entries):
+    db_cursor = db_conn.cursor()
+    insert_statement = sql.insert(TABLE_NAME, TABLE_FIELDS)
+    for entry in entries:
+        db_cursor.execute(insert_statement, entry)
+    db_conn.commit()
--- src/fs/__init__.py
+++ src/fs/__init__.py
@@ -1,1 +1,9 @@
+# Copyright 2006 Uberan - All Rights Reserved
+
+from FileHistoryStore import FileHistoryStore, FileHistoryEntry
 from FileSystem import FileSystem, join_paths
+
+# returns whether mtimes are within 1 sec of each other, because
+# Windows shaves off a bit of mtime info.
+def mtimes_eq(mtime1, mtime2):
+    return abs(mtime1 - mtime2) < 2
